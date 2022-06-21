cc2943f8d28e0d1cc3de9d53655efda8e8af7ab3
Victor_Santos <107374148+VictorL-Santos@users.noreply.github.com>
2022-06-21 18:56:54 -0300

    Create LEDSequence.ino

---
+++ LEDSequence/LEDSequence.ino
@@ -0,0 +1,610 @@
+int led1 = 13;
+int led2 = 12;
+int led3 = 11;
+int led4 = 10;
+int led5 = 9;
+int led6 = 8;
+int led7 = 7;
+int led8 = 6;
+int led9 = 5;
+int rgbR = 4;
+int rgbB = 3;
+int rgbG = 2;
+
+void setup()
+{
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+  pinMode(led6, OUTPUT);
+  pinMode(led7, OUTPUT);
+  pinMode(led8, OUTPUT);
+  pinMode(led9, OUTPUT);
+  pinMode(rgbR, OUTPUT);
+  pinMode(rgbB, OUTPUT);
+  pinMode(rgbG, OUTPUT);
+  
+}
+
+void loop()
+{
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, HIGH);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, HIGH);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, HIGH);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, HIGH);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, HIGH);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, HIGH);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  //etapa 2
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+  digitalWrite(led5, HIGH);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+  digitalWrite(led5, HIGH);
+  digitalWrite(led6, HIGH);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+  digitalWrite(led5, HIGH);
+  digitalWrite(led6, HIGH);
+  digitalWrite(led7, HIGH);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+  digitalWrite(led5, HIGH);
+  digitalWrite(led6, HIGH);
+  digitalWrite(led7, HIGH);
+  digitalWrite(led8, HIGH);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+  digitalWrite(led5, HIGH);
+  digitalWrite(led6, HIGH);
+  digitalWrite(led7, HIGH);
+  digitalWrite(led8, HIGH);
+  digitalWrite(led9, HIGH);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, 255);
+  analogWrite(rgbB, 255);
+  analogWrite(rgbG, 255);
+  delay(500);
+  
+  //etapa 3
+  
+  on();
+  
+  off();
+  
+  on();
+  
+  off();
+  
+  on();
+  
+  //etapa4
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+  digitalWrite(led5, HIGH);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+  digitalWrite(led5, HIGH);
+  digitalWrite(led6, HIGH);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+  digitalWrite(led5, HIGH);
+  digitalWrite(led6, HIGH);
+  digitalWrite(led7, HIGH);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+  digitalWrite(led5, HIGH);
+  digitalWrite(led6, HIGH);
+  digitalWrite(led7, HIGH);
+  digitalWrite(led8, HIGH);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+  digitalWrite(led5, HIGH);
+  digitalWrite(led6, HIGH);
+  digitalWrite(led7, HIGH);
+  digitalWrite(led8, HIGH);
+  digitalWrite(led9, HIGH);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+  digitalWrite(led5, HIGH);
+  digitalWrite(led6, HIGH);
+  digitalWrite(led7, HIGH);
+  digitalWrite(led8, HIGH);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+  digitalWrite(led5, HIGH);
+  digitalWrite(led6, HIGH);
+  digitalWrite(led7, HIGH);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+  digitalWrite(led5, HIGH);
+  digitalWrite(led6, HIGH);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+  digitalWrite(led5, HIGH);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(500);
+  
+  //etapa 5
+  
+  on();
+  
+  off();
+  
+  on();
+  
+  off();
+  
+  on();
+  
+}
+
+void on() {
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+  digitalWrite(led5, HIGH);
+  digitalWrite(led6, HIGH);
+  digitalWrite(led7, HIGH);
+  digitalWrite(led8, HIGH);
+  digitalWrite(led9, HIGH);
+  analogWrite(rgbR, 255);
+  analogWrite(rgbB, 255);
+  analogWrite(rgbG, 255);
+  delay(500);
+}
+
+void off() {
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  digitalWrite(led8, LOW);
+  digitalWrite(led9, LOW);
+  analogWrite(rgbR, LOW);
+  analogWrite(rgbB, LOW);
+  analogWrite(rgbG, LOW);
+  delay(300);
+}
